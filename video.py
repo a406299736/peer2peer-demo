29b1f940c9b73d95a489323b3e9edaf3ad350574
jinsiyuan <jinsiyuan@58ganji.com>
2018-10-16 12:23:48 +0800

    rn

--- video.py
+++ video.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route('/video')
 def video():
     return render_template('video.html')
+
+
+if __name__ == '__main__':
+    app.env = ''
+    app.run('120.27.55.146',port=9997, debug=True)
